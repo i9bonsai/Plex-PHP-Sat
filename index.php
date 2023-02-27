babd5d6f0f27f1fed98f4e1db3d54efd712ff4b9
i9bonsai <kupa.vx@gmail.com>
2023-02-28 09:40:47 +1100

    Adding to basic framework and grounds

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Interlude V1</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel="stylesheet" href="/main.css">
+    <script src='main.js'></script>
+</head>
+<body>
+    <div class="overlay">
+        <div class="loading-box">
+            <img class="centred" src=imgs/tumblr_793685cd248e27e31e2a3f681b5bd83f_0392115f_250.gif alt="ghost.gif" width="150" height="150">
+            
+        </div>
+    </div>
+    <p>meow!</p>
+    
+    <?php
+
+    ?>
+    
+</body>
+</html>
