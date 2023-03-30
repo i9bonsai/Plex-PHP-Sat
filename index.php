1dd85e296bb068b22711b3d39f6533cfe616f916
i9bonsai <kupa.vx@gmail.com>
2023-03-30 13:41:21 +1100

    yep

--- index.php
+++ index.php
@@ -5,16 +5,26 @@
     <title>Interlude V1</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <link rel="stylesheet" href="/main.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@600&display=swap" rel="stylesheet">
     <script src='main.js'></script>
 </head>
 <body>
     <div class="overlay">
         <div class="loading-box">
-            <img class="centred" src=imgs/tumblr_793685cd248e27e31e2a3f681b5bd83f_0392115f_250.gif alt="ghost.gif" width="150" height="150">
-            
+            <div class="loading-logo">
+                <img src="/imgs/tumblr_793685cd248e27e31e2a3f681b5bd83f_0392115f_250.gif" alt="logo">
+            </div>
+            <div class="loading-text">
+                Loading...
+            </div>
+        </div>
+
         </div>
     </div>
-    <p>meow!</p>
+
+    
     
     <?php
 
